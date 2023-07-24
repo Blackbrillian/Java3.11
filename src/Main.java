3dd30c1b727f15df7d4681a74f4d715f9739b0c0
Elvina <krivileva19@bk.ru>
2023-07-24 21:29:20 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 200;
+        int percent = 5;
+        int bonus = amount * percent / 100;
+        System.out.println("начисленные Бонусы за покупку:"+bonus);
+}
+}
+
